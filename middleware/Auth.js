a1cdff4e4bb7b366a59b3af4f880d0b83d81f8cf
Yaser12345 <yaser.karami1995@gmail.com>
2022-06-19 14:04:19 +0430

    c

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,7 +1,22 @@
+import jwt from 'jsonwebtoken'
+import {UnAuthenticatedError} from "../errors/index.js"
+
 const Auth = async(req,res,next) => {
     const authHeader = req.headers.authorization
-    console.log(authHeader)
+    if(!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnAuthenticatedError('Authentication Invalid!')
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = {userId: payload.userId}
+        console.log(payload);
     next()
+        
+    } catch (error) {
+        throw new UnAuthenticatedError('Authentication Invalid!')
+        
+    }
 }
 
 export default Auth
--- models/User.js
+++ models/User.js
@@ -45,7 +45,7 @@ UserSchema.pre("save", async function () {
 });
 
 UserSchema.methods.createJWT = function () {
-  return JWT.sign({ id: this._id }, process.env.JWT_SECRET, {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   });
 };
